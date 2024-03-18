3d1ff7b70fe0c7cc1964181fb09b7c15e8abe3f6
Malo <malo.nicolas-coqueron@grenoble-inp.org>
2024-03-19 00:15:28 +0100

    nouvel algo pour chercher les uinclusions

--- find.py
+++ find.py
@@ -3,16 +3,52 @@ from geo.polygon import Polygon
 from geo.point import Point
 from geo.segment import Segment
 from ray_casting import RayCast
+from tree import Tree
+
+#variables gloables
+IN = True
+OUT = False
 
 class Find:
 
+    @staticmethod
+    def __poly_abs_extremis(polygones):
+        liste = []
+        for polygone in enumerate(polygones):
+            point_min,point_max = polygone[1].bounding_quadrant().limits(0)
+            liste.append((point_min,IN,polygone[0]))
+            liste.append((point_max,OUT,polygone[0]))
+        return sorted(liste)
+
+    @staticmethod
+    def __get_potential_tree(liste_point):
+        current = Tree()
+        forest = []
+        for (_, IN, id) in liste_point:
+            new_branch = Tree()
+            if current.parent :
+                if IN:
+                    current.set_value(id)
+                    current.add_childs(new_branch)
+                else:
+                    while current.value != id:
+                        current = current.parent
+                    if current.parent is not None:
+                        current.parent.add_childs(new_branch)
+            else:
+                current.set_value(id)
+                current.add_childs(new_branch)
+                forest.append(current)
+            current = new_branch
+        return forest
+    
+    @staticmethod
     def __list_with_area(polygones):
         tab = []
         for (i,polygone) in enumerate(polygones):
             tab.append((abs(polygone.area()),i))
         return(tab)
 
-    @staticmethod
     def naif(polygones):
         inclusions : list = [-1 for _ in range(len(polygones))]
         for i, polygon1 in enumerate(polygones):
@@ -34,3 +70,13 @@ class Find:
                     inclusions[polygone1] = polygone2
                     break
         return inclusions
+
+    def area_local_vision(polygones):
+        liste = Find.__poly_abs_extremis(polygones)
+        forest_inclusion = Find.__get_potential_tree(liste)
+        #print(forest_inclusion[0])
+        inclusions : list = [-1 for _ in range(len(polygones))]
+        for tree in forest_inclusion:
+            tree.test_inclusion_with_childs(inclusions,polygones)
+        return inclusions
+
---
+++ tree.py
@@ -0,0 +1,41 @@
+from geo.polygon import Polygon
+from geo.point import Point
+from ray_casting import RayCast
+
+class Tree:
+
+    def __init__(self):
+        self.parent = None
+        self.value = None
+        self.childs = []
+
+    def __str__(self):
+        char = ""
+        if self.value is not None :
+            char += str(self.value) + " -> [ "
+            for child in self.childs:
+                char += str(child)
+            char += " ]"
+        return char
+
+    def set_value(self,value):
+        self.value = value
+
+    def add_childs(self,child):
+        self.childs.append(child)
+        child.parent = self
+        
+    def set_parent(self,parent):
+        self.parent = parent
+
+    def test_inclusion_with_childs(self,inclusions,polygones):
+        i = self.value
+        for child in self.childs:
+            if child.value != None:
+                #print(child)
+                j = child.value
+                #print("test ", j , " dans ",i)
+                if RayCast.is_include(polygones[j],polygones[i]):
+                    inclusions[j] = i
+                    #print("pass")
+                child.test_inclusion_with_childs(inclusions,polygones)
